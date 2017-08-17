6a746dbac4a83fb6c7839e3d867f7ebf9aba433b
wuzhao <liwuzhao97@gmail.com>
2017-08-17 23:11:32 +0800

    User login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,18 @@ class User < ApplicationRecord
 
   validates_presence_of :email, message: "邮箱不能为空"
   validates :email, uniqueness: true
+  validates_format_of :email, message: "邮箱格式不合法",
+                      with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+                      if: proc { |user| !user.email.blank? }
 
-  validates_presence_of :password, :message: "密码不能为空", if: :need_validates_password
-  validates_presence_of :password_confirmation, :message: "密码确认不能为空", if: :need_validates_password
-  validates_confirmation_of :password, :message: "两次密码输入不一致", if: :need_validates_password
-  validates_lenght_of :password, :message "密码最短为6位", minimum: 6, if: :need_validates_password
+  validates_presence_of :password, message: "密码不能为空", if: :need_validates_password
+  validates_presence_of :password_confirmation, message: "密码确认不能为空", if: :need_validates_password
+  validates_confirmation_of :password, message: "两次密码输入不一致", if: :need_validates_password
+  validates_length_of :password, message: "密码最短为6位", minimum: 6, if: :need_validates_password
+
+  def username
+    self.email.split('@').first
+  end
 
   private
     def need_validates_password
